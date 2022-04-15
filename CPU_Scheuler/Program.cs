f3ad9fb49701a7c4baf6e008e7ad94670a9775e6
AmrMohsen122 <84917265+AmrMohsen122@users.noreply.github.com>
2022-04-15 02:47:08 +0200

    test for SJF preemptive

--- CPU_Scheuler/Program.cs
+++ CPU_Scheuler/Program.cs
@@ -6,20 +6,27 @@ namespace CPU_Scheduler
     {
         public static void Main(String[] args)
         {
-           Process p1=new Process(1,0,7);
-            Process p2 = new Process(2, 3, 2);
-            Process p3 = new Process(3, 3, 5);
-            Process p4 = new Process(4, 5, 3);
-            Process p5 = new Process(5, 4, 1);
+           Process p1=  new Process(1,0,8);
+            Process p2 = new Process(2, 0.3f, 4);
+            Process p3 = new Process(3, 13.5f, 9);
+            Process p4 = new Process(4, 23.3f, 5);
+           
 
-            List<Process> processes = new List<Process>();      
-            processes.Add(p1);
+            List<Process> processes = new List<Process>();     
             processes.Add(p2);
+            processes.Add(p1);
+            
             processes.Add(p3);
             processes.Add(p4);
-            processes.Add(p5);
+
+           
             Console.WriteLine("pid : completetion time");
-            Console.WriteLine(SJF.schedule(processes, 5));
+            SJF.schedule_prem(processes, 4);
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("  "+processes[i].getPid() +" : "+ processes[i].getCompletetionTime());
+            }
+            
             
           
         }

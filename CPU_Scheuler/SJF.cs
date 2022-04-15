9548ef786b1b73c83566be6fd81dad4a2a21e8b2
AmrMohsen122 <84917265+AmrMohsen122@users.noreply.github.com>
2022-04-15 02:46:07 +0200

    add the code for SJF preemptive

--- CPU_Scheuler/SJF.cs
+++ CPU_Scheuler/SJF.cs
@@ -49,7 +49,71 @@ namespace CPU_Scheduler
                 Console.WriteLine("  "+p[min].getPid()+" : "+p[min].getCompletetionTime());
                 counter++;
             }
-           
+           public static void schedule_prem(List<Process> p, int n)
+        {
+            float currentTime = p[0].getArrivalTime(); ;
+            float nextArrivalTime = p[0].getArrivalTime();
+            //point to the end of the arrived processes
+            int turn = 0;
+            int left = 0;
+            //number of completed processes
+            int completed = 0;
+            //heap
+            PriorityQueue<Process, float> priorityQueue = new PriorityQueue<Process, float>();
+            Process currentRuning;
+            //sort the processes according to their arrival time
+            p.Sort((x, y) => x.getArrivalTime().CompareTo(y.getArrivalTime()));
+            while (true)
+            {
+                //exit loop with the last process that has the same arrival time
+                while (turn < n && nextArrivalTime == p[turn].getArrivalTime() )
+                {
+                    turn++;
+                }
+                if (turn < n)
+                    nextArrivalTime = p[turn].getArrivalTime();
+                //find the shortest remaining time in the current arrivals
+                for (int i = left; i < turn; i++)
+                {
+                    priorityQueue.Enqueue(p[i], p[i].getBurstTime());
+                }
+                left = turn;
+
+                while (currentTime != nextArrivalTime)
+                {
+                    if(priorityQueue.Count == 0)
+                    {
+                        currentTime = nextArrivalTime;
+                        break;
+                    } 
+                    currentRuning = priorityQueue.Dequeue();
+                    if ((currentRuning.getBurstTime()) > (nextArrivalTime - currentTime))
+                    {
+                        currentRuning.setBurstTime(currentRuning.getBurstTime() - (nextArrivalTime - currentTime));
+                        priorityQueue.Enqueue(currentRuning, currentRuning.getBurstTime());
+                        currentTime = nextArrivalTime;
+                    }
+                    else
+                    {
+                        currentRuning.setCompletetionTime(currentRuning.getBurstTime()+currentTime);
+                        completed++;
+                        currentTime = currentRuning.getBurstTime() + currentTime;
+                    }
+                    
+                }
+                if (turn >= n)
+                {
+                    break;
+                }
+            }
+            while (completed != n)
+            {
+                currentRuning = priorityQueue.Dequeue();
+                currentTime += currentRuning.getBurstTime();
+                currentRuning.setCompletetionTime(currentTime);
+                completed++;
+            }
+        }
 
 
 
